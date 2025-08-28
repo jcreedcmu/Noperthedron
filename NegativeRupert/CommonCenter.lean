c69c22052b2ab117b8f70a6a030686906b45793f
Jason Reed <jcreed@gmail.com>
2025-08-28 08:39:37 -0400

    Finished proving this lemma

--- NegativeRupert/CommonCenter.lean
+++ NegativeRupert/CommonCenter.lean
@@ -3,26 +3,17 @@ import Rupert.Basic
 def PointSym (A : Set ℝ²) : Prop :=
  ∀ x ∈ A, -x ∈ A
 
-lemma foo (k : ℝ) (a v : ℝ²) : k • a + k • v + k • (a - v) = (k * 2) • a := by
+lemma segment_lemma (k : ℝ) (a v : ℝ²) : k • a + k • v + k • (a - v) = (k * 2) • a := by
  rw [smul_sub]
  calc (k • a + k • v) + (k • a - k • v)
- _ = k • a + (k • v + (k • a - k • v)) := by apply add_assoc
+ _ = k • a + (k • v + (k • a - k • v)) := by rw [add_assoc]
  _ = k • a + (k • v + ((-(k • v)) + k • a)) := by nth_rw 5 [add_comm]; rfl
  _ = k • a + ((k • v + (-(k • v))) + k • a) := by nth_rw 1 [add_assoc]
- _ = (k * 2) • a := by sorry
-
-
-     -- calc (1/2) • a + (1/2) • v + (1/2) • (a - v)
-     --       = (1/2) • a + (1/2) • v + ((1/2) • a - (1/2) • v) : by rw smul_sub
-     --   ... = (1/2) • a + (1/2) • v + (1/2) • a - (1/2) • v : by rw add_assoc
-     --   ... = (1/2) • a + (1/2) • a + ((1/2) • v - (1/2) • v) : by ac_refl
-     --   ... = (1/2) • a + (1/2) • a + 0 : by rw sub_self
-     --   ... = (1/2 + 1/2) • a : by rw add_smul
-     --   ... = 1 • a : by norm_num
-     --   ... = a : by rw one_smul
+ _ = k • a + k • a := by rw [add_neg_cancel, zero_add]
+ _ = (k * 2) • a := by rw [← add_smul, ← mul_two]
 
 /--
-Suppose A and B are both pointsymmetric. Suppose B is convex.
+Suppose A and B are both pointsymmetric subsets of ℝ². Suppose B is convex.
 If some translate of A is contained in B, then A is contained in B.
 -/
 theorem common_center (A B : Set ℝ²) (psa : PointSym A) (psb : PointSym B)
@@ -30,19 +21,22 @@ theorem common_center (A B : Set ℝ²) (psa : PointSym A) (psb : PointSym B)
     (v : ℝ²) (hin : (· + v) '' A ⊆ B) : A ⊆ B := by
   intro a ha
   have h1 : a + v ∈ B := by
-    sorry
+    grind only [= Set.mem_image, = Set.subset_def]
   have h2 : a - v ∈ B := by
-    sorry
-  have z := convex_iff_segment_subset.mp b_convex h1 h2
-  have q : a ∈ segment ℝ (a + v) (a - v) := by
+    have han : -a ∈ A := psa a ha
+    have hnav : -a + v ∈ B := by grind only [= Set.mem_image, = Set.subset_def]
+    have hnn : -(-a + v) ∈ B := psb (-a + v) hnav
+    have e : -(-a + v) = a - v := by grind only
+    rw [e] at hnn
+    exact hnn
+  have segment_sub_b := convex_iff_segment_subset.mp b_convex h1 h2
+  have a_in_segment : a ∈ segment ℝ (a + v) (a - v) := by
     unfold segment
     simp
     refine ⟨ 1/2, ?_, 1/2, ?_, ?_, ?_ ⟩
     · simp only [one_div, inv_nonneg, Nat.ofNat_nonneg]
     · simp only [one_div, inv_nonneg, Nat.ofNat_nonneg]
     · grind
-    · rw [foo (1/2) a v]
-
-
-      sorry
-  sorry
+    · rw [segment_lemma (1/2) a v]
+      norm_num
+  exact segment_sub_b a_in_segment
