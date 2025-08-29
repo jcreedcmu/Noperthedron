6e98762ee6d4a1befed9236ccf611e9aaadab916
Jason Reed <jcreed@gmail.com>
2025-08-29 09:57:02 -0400

    Some more rotation-elimination progress

--- NegativeRupert/CommonCenter.lean
+++ NegativeRupert/CommonCenter.lean
@@ -1,6 +1,8 @@
 import Rupert.Basic
+import Rupert.Set
+import NegativeRupert.RotRupert
 
-def PointSym (A : Set ℝ²) : Prop :=
+def PointSym {n : ℕ} (A : Set (Fin n → ℝ)) : Prop :=
  ∀ x ∈ A, -x ∈ A
 
 lemma segment_lemma (k : ℝ) (a v : ℝ²) : k • a + k • v + k • (a - v) = (k * 2) • a := by
@@ -16,7 +18,7 @@ lemma segment_lemma (k : ℝ) (a v : ℝ²) : k • a + k • v + k • (a - v) 
 Suppose A and B are both pointsymmetric subsets of ℝ². Suppose B is convex.
 If some translate of A is contained in B, then A is contained in B.
 -/
-theorem common_center (A B : Set ℝ²) (psa : PointSym A) (psb : PointSym B)
+theorem common_center {A B : Set ℝ²} (psa : PointSym A) (psb : PointSym B)
     (b_convex : Convex ℝ B)
     (v : ℝ²) (hin : (· + v) '' A ⊆ B) : A ⊆ B := by
   intro a ha
@@ -32,7 +34,7 @@ theorem common_center (A B : Set ℝ²) (psa : PointSym A) (psb : PointSym B)
   have segment_sub_b := convex_iff_segment_subset.mp b_convex h1 h2
   have a_in_segment : a ∈ segment ℝ (a + v) (a - v) := by
     unfold segment
-    simp
+    simp only [smul_add, exists_and_left, Set.mem_setOf_eq]
     refine ⟨ 1/2, ?_, 1/2, ?_, ?_, ?_ ⟩
     · simp only [one_div, inv_nonneg, Nat.ofNat_nonneg]
     · simp only [one_div, inv_nonneg, Nat.ofNat_nonneg]
@@ -40,3 +42,20 @@ theorem common_center (A B : Set ℝ²) (psa : PointSym A) (psb : PointSym B)
     · rw [segment_lemma (1/2) a v]
       norm_num
   exact segment_sub_b a_in_segment
+
+/--
+If a set is point symmetric, then being rupert implies being purely rotationally rupert.
+-/
+theorem rupert_implies_rot_rupert {S : Set ℝ³} (s_sym : PointSym S)
+    (r : IsRupertSet S) : IsRotRupertSet S := by
+  unfold IsRupertSet IsRupertPair at r
+  obtain ⟨inn, inn_so3, off, out, out_so3, v⟩ := r
+  use inn, inn_so3, out, out_so3
+  intro inner_shadow outer_shadow
+  let inner_shadow' := {x | ∃ p ∈ S, off + proj_xy (inn.mulVec p) = x}
+  change closure inner_shadow' ⊆ interior outer_shadow at v
+  refine common_center ?_ ?_ ?_ off ?_
+  · sorry
+  · sorry
+  · sorry
+  · sorry
---
+++ NegativeRupert/RotRupert.lean
@@ -0,0 +1,20 @@
+import Rupert.Basic
+
+open scoped Matrix
+
+/-- The Rotational. Rupert Property for a pair of subsets X, Y of ℝ³. X has the
+    Rupert property with respect to Y if there such that the shadow of
+    X fits "comfortably" within the shadow of Y under rotations.
+    By "comfortably" we mean the closure of one set is
+    a subset of the interior of the other. This definition rules out
+    trivial cases of a set fitting inside itself. -/
+def IsRotRupertPair (inner outer : Set ℝ³) : Prop :=
+   ∃ inner_rot ∈ SO3, ∃ outer_rot ∈ SO3,
+   let inner_shadow := { proj_xy (inner_rot *ᵥ p) | p ∈ inner }
+   let outer_shadow := { proj_xy (outer_rot *ᵥ p) | p ∈  outer }
+   closure inner_shadow ⊆ interior outer_shadow
+
+/-- The Rupert Property for a subset S of ℝ³. S has the Rupert property if there
+    are rotations and translations such that one 2-dimensional "shadow" of S can
+    be made to fit entirely inside the interior of another such "shadow". -/
+def IsRotRupertSet (S : Set ℝ³) : Prop := IsRotRupertPair S S
