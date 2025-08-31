333e8fc7262055062872d8a1119a0a2ac4897b41
Jason Reed <jcreed@gmail.com>
2025-08-31 11:30:14 -0400

    Progress towards proving nopert pointsymmetric

--- NegativeRupert/Nopert.lean
+++ NegativeRupert/Nopert.lean
@@ -42,13 +42,15 @@ theorem c2_norm_bound : ‖C2‖ ∈ Set.Ioo (98/100) (99/100) := by
 theorem c3_norm_bound : ‖C3‖ ∈ Set.Ioo (98/100) (99/100) := by
   sorry -- TODO(easy)
 
+/-- This is half of the C30 defined in [SY25]. In order
+to see that this is pointsymmetric, it's convenient to
+do explicit pointsymmetrization later. -/
 noncomputable
-def C30 : List (Matrix (Fin 3) (Fin 3) ℝ) := do
+def C15 : List (Matrix (Fin 3) (Fin 3) ℝ) := do
   let k ← List.range 15
-  let ℓ ← List.range 2
-  pure ((-1)^ℓ * (Rz (2 * π * k / 15)))
+  pure (Rz (2 * π * k / 15))
 
-lemma length_c30 : C30.length = 30 := by simp [C30]
+lemma length_c15 : C15.length = 15 := by simp [C15]
 
 end Nopert
 
@@ -56,30 +58,58 @@ end Nopert
 The noperthedron, given as a finite list of vertices.
 -/
 noncomputable
+def halfNopertVertList : List ℝ³ :=
+    List.map (· *ᵥ Nopert.C1R) Nopert.C15 ++
+    List.map (· *ᵥ Nopert.C2R) Nopert.C15 ++
+    List.map (· *ᵥ Nopert.C3R) Nopert.C15
+
+def pointsymmetrize (ℓ : List ℝ³) : List ℝ³ := ℓ ++ ℓ.map (fun x => -x)
+
+noncomputable
 def nopertVertList : List ℝ³ :=
-    List.map (· *ᵥ Nopert.C1R) Nopert.C30 ++
-    List.map (· *ᵥ Nopert.C2R) Nopert.C30 ++
-    List.map (· *ᵥ Nopert.C3R) Nopert.C30
+  pointsymmetrize halfNopertVertList
 
 def nopertNumVerts : ℕ := 90
 
 lemma nopert_vert_list_length : nopertVertList.length = nopertNumVerts := by
   unfold nopertVertList
-  simp only [List.length_append, List.length_map, Nopert.length_c30]
+  simp only [List.length_append, List.length_map, pointsymmetrize]
   rfl
 
 /--
 The noperthedron, given as a finite set of vertices.
 -/
 noncomputable
-def nopertVerts (i : Fin nopertNumVerts) : ℝ³ := by
-  rw [← nopert_vert_list_length] at i
-  exact nopertVertList.get i
+def nopertVerts : Fin nopertVertList.length → ℝ³ := nopertVertList.get
+
+/--
+The noperthedron, given as a set of vertices.
+-/
+noncomputable
+def nopertVertSet : Set ℝ³ := Set.range nopertVerts
 
 noncomputable
 def nopert : Shape where
   size := nopertNumVerts
   vertices := nopertVerts
+
+lemma pointsymmetrization_is_pointsym (ℓ : List ℝ³) :
+    PointSym { x | x ∈ pointsymmetrize ℓ } := by
+  intro a ha
+  refine List.mem_append.mpr ?_
+  cases List.mem_append.mp ha with
+  | inl ha => exact Or.inr (List.mem_map_of_mem ha)
+  | inr ha =>
+    refine Or.inl ?_
+    have ⟨c, hc, e⟩ := List.exists_of_mem_map ha
+    rw [← e]
+    simp only [neg_neg]
+    exact hc
+
+theorem nopert_verts_pointsym : PointSym nopertVertSet := by
+  rw [nopertVertSet, nopertVerts, Set.range_list_get]
+  simp only [nopertVertList]
+  apply pointsymmetrization_is_pointsym
 
 theorem nopert_point_symmetric : PointSym nopert.hull := by
   sorry -- TODO(medium)
