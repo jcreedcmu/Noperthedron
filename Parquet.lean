72d3af6b5caadd1c9346174f13319789b2124f01
Jason Reed <jcreed@gmail.com>
2025-09-14 11:30:51 -0400

    fseek from end works

--- Parquet.lean
+++ Parquet.lean
@@ -1,17 +1,25 @@
 structure Parquet where
 
 @[extern "lean_io_fs_handle_seek"]
--- opaque IO.FS.Handle.seek (h : @& IO.FS.Handle) (offset : Int) (whence : UInt32) : IO UInt64
-opaque IO.FS.Handle.seek (h : @& IO.FS.Handle) (offset : UInt32) : IO UInt32
+opaque IO.FS.Handle.seek (h : @& IO.FS.Handle) (offset : UInt64) (whence : Int32) : IO Int32
+
+@[extern "lean_io_fs_SEEK_SET"]
+opaque IO.FS.SEEK_SET : Unit → Int32
+
+@[extern "lean_io_fs_SEEK_CUR"]
+opaque IO.FS.SEEK_CUR : Unit → Int32
+
+@[extern "lean_io_fs_SEEK_END"]
+opaque IO.FS.SEEK_END : Unit → Int32
 
 def readParquetFile (path : System.FilePath) : IO Parquet := do
 
   if (!(← System.FilePath.pathExists path)) then
     throw (IO.userError "Path '{dataPath}' relative to project root does not exist")
   let _contents ← IO.FS.withFile path IO.FS.Mode.read (fun f => do
-    let z ← f.seek 8
+    let z ← f.seek (-16) (IO.FS.SEEK_END ())
     IO.println s!"Got back integer {z} from FFI"
-    let buf ← f.read 8
+    let buf ← f.read 16
     IO.println s!"Here's a buffer: {buf}"
   )
   IO.println "Success"
--- ffi/ffi_seek.c
+++ ffi/ffi_seek.c
@@ -1,19 +1,24 @@
 #include <lean/lean.h>
 #include <stdio.h>
 
-extern lean_object* lean_io_fs_handle_seek(lean_object *handle, uint32_t offset) {
+
+int32_t lean_io_fs_SEEK_SET() { return SEEK_SET; }
+int32_t lean_io_fs_SEEK_CUR() { return SEEK_CUR; }
+int32_t lean_io_fs_SEEK_END() { return SEEK_END; }
+
+extern lean_object* lean_io_fs_handle_seek(lean_object *handle, uint64_t offset, int32_t whence) {
   FILE *fp = (FILE *)(lean_get_external_data(handle));
-  printf("Seeking...\n");
-  int result = fseek(fp, offset, SEEK_SET);
+  int32_t result = fseek(fp, offset, whence);
   return lean_io_result_mk_ok(lean_box(result));
-  /* FILE* fp = lean_to_io_handle(h); */
-  /* if (fp == NULL) { */
-  /*     return lean_io_result_mk_error(lean_mk_io_user_error("invalid handle"), w); */
-  /* } */
-  /* long long off = lean_unbox_int(offset); */
-  /* if (fseeko(fp, off, whence) != 0) { */
-  /*     return lean_io_result_mk_error(lean_mk_io_user_error("seek failed"), w); */
-  /* } */
-  /* off_t pos = ftello(fp); */
-  /* return lean_io_result_mk_ok(lean_box_uint64((uint64_t)pos), w); */
 }
+
+/* FILE* fp = lean_to_io_handle(h); */
+/* if (fp == NULL) { */
+/*     return lean_io_result_mk_error(lean_mk_io_user_error("invalid handle"), w); */
+/* } */
+/* long long off = lean_unbox_int(offset); */
+/* if (fseeko(fp, off, whence) != 0) { */
+/*     return lean_io_result_mk_error(lean_mk_io_user_error("seek failed"), w); */
+/* } */
+/* off_t pos = ftello(fp); */
+/* return lean_io_result_mk_ok(lean_box_uint64((uint64_t)pos), w); */
