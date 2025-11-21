d54c5a8adb35dd8f755760a384095437ab0e135b
David Renshaw <dwrenshaw@gmail.com>
2025-11-21 11:38:52 -0500

    norm_M_apply_gt

--- NegativeRupert/Bounding.lean
+++ NegativeRupert/Bounding.lean
@@ -6,17 +6,22 @@ open scoped Real
 
 namespace Bounding
 
-theorem one_plus_cos_mul_one_plus_cos_ge (a b : ℝ) (ha : |a| ≤ 2) (hb : |b| ≤ 2) :
+theorem one_plus_cos_mul_one_plus_cos_ge {a b : ℝ} (ha : |a| ≤ 2) (hb : |b| ≤ 2) :
     2 + 2 * Real.cos √(a ^ 2 + b ^ 2) ≤ (1 + Real.cos a) * (1 + Real.cos b) := by
   sorry
 
-theorem norm_RxRy_minus_id_le (α β : ℝ) : ‖RxL α ∘L RyL β - 1‖ ≤ √(α ^ 2 + β ^ 2) := by
+theorem norm_RxRy_minus_id_le {α β : ℝ} : ‖RxL α ∘L RyL β - 1‖ ≤ √(α ^ 2 + β ^ 2) := by
   sorry
 
-theorem norm_M_sub_lt (ε θ θ_ φ φ_ : ℝ) (hε : 0 < ε) (hθ : |θ - θ_| ≤ ε) (hφ : |φ - φ_| ≤ ε) :
+theorem norm_M_sub_lt {ε θ θ_ φ φ_ : ℝ} (hε : 0 < ε) (hθ : |θ - θ_| ≤ ε) (hφ : |φ - φ_| ≤ ε) :
     ‖rotM θ φ - rotM θ_ φ_‖ < √2 * ε := by
   sorry
 
-theorem norm_X_sub_lt (ε θ θ_ φ φ_ : ℝ) (hε : 0 < ε) (hθ : |θ - θ_| ≤ ε) (hφ : |φ - φ_| ≤ ε) :
+theorem norm_X_sub_lt {ε θ θ_ φ φ_ : ℝ} (hε : 0 < ε) (hθ : |θ - θ_| ≤ ε) (hφ : |φ - φ_| ≤ ε) :
     ‖rotX θ φ - rotX θ_ φ_‖ < √2 * ε := by
+  sorry
+
+theorem norm_M_apply_gt {ε r θ θ_ φ φ_ : ℝ} (P : ℝ³)
+    (hP : ‖P‖ ≤ 1) (hε : 0 < ε) (hr : 0 < r) (hθ : |θ - θ_| ≤ ε) (hφ : |φ - φ_| ≤ ε)
+    (hM : r + √2 * ε < ‖rotM θ_ φ_ P‖) : r < ‖rotM θ φ P‖ := by
   sorry
