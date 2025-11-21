35503e3ad62bccc94021df5d54c0ac99f0eda37f
David Renshaw <dwrenshaw@gmail.com>
2025-11-20 19:32:31 -0500

    circ symbol for comp

--- NegativeRupert/Bounding.lean
+++ NegativeRupert/Bounding.lean
@@ -10,9 +10,9 @@ theorem one_plus_cos_mul_one_plus_cos_ge (a b : ℝ) (ha : |a| ≤ 2) (hb : |b| 
     2 + 2 * Real.cos √(a ^ 2 + b ^ 2) ≤ (1 + Real.cos a) * (1 + Real.cos b) := by
   sorry
 
-theorem norm_RxRy_minus_id_le (α β : ℝ) : ‖(RxL α).comp (RyL β) - 1‖ ≤ √(α ^ 2 + β ^ 2) := by
+theorem norm_RxRy_minus_id_le (α β : ℝ) : ‖RxL α ∘L RyL β - 1‖ ≤ √(α ^ 2 + β ^ 2) := by
   sorry
 
 theorem norm_RxRy_minus_id_eq_iff (α β : ℝ) :
-    ‖(RxL α).comp (RyL β) - 1‖ = √(α ^ 2 + β ^ 2) ↔ α = 0 ∧ β = 0 := by
+    ‖RxL α ∘L RyL β - 1‖ = √(α ^ 2 + β ^ 2) ↔ α = 0 ∧ β = 0 := by
   sorry
