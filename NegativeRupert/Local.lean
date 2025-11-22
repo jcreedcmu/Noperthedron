f4a12354193602d4e72ea3c61293a515ee4a1602
Jason Reed <jcreed@gmail.com>
2025-11-22 09:17:13 -0500

    Finish statement of Local Theorem

--- NegativeRupert/Local.lean
+++ NegativeRupert/Local.lean
@@ -79,16 +79,55 @@ theorem coss {δ ε θ θ_ φ φ_ : ℝ} {P Q : Euc(3)}
      ⟪M P, M (P - Q)⟫ / (‖M P‖ * ‖M (P - Q)‖):= by
   sorry
 
-def Triangle.Az (X : ℝ³) (P : Triangle) (ε : ℝ) : Prop :=
+/--
+Condition A_ε from [SY25] Theorem 36
+-/
+def Triangle.Aε (X : ℝ³) (P : Triangle) (ε : ℝ) : Prop :=
   ∃ σ ∈ ({-1, 1} : Set ℝ), ∀ i : Fin 3, ⟪X, P i⟫ > ε * √2
 
+noncomputable
+def Triangle.Bε.lhs (i j : Fin 3) (Q : Triangle) (p : LooseViewPose) (ε : ℝ) : ℝ :=
+   (⟪p.rotM₂ (Q i), p.rotM₂ (Q i - Q j)⟫ - 2 * ε * ‖Q i - Q j‖ * (ε + √2))
+   / ((‖p.rotM₂ (Q i)‖ + ε * √2) * (‖p.rotM₂ (Q i - Q j)‖ + 2 * ε * √2))
 
+/--
+Condition B_ε from [SY25] Theorem 36
+-/
+def Triangle.Bε (Q : Triangle) (p : LooseViewPose) (ε δ r : ℝ) : Prop :=
+  ∀ i j : Fin 3, i ≠ j →
+  Triangle.Bε.lhs i j Q p ε > (δ + ε * √5) / r
+
+instance : Membership Triangle (Finset ℝ³) where
+  mem set tri := ∀ i : Fin 3, (tri i) ∈ set
+
+def BoundDelta (δ : ℝ) (p : LooseViewPose) (P Q : Triangle) : Prop :=
+  ∀ i : Fin 3, δ ≥ ‖p.rotR (p.rotM₁ (P i)) - p.rotM₂ (Q i)‖/2
+
+def BoundR (r ε : ℝ) (p : LooseViewPose) (Q : Triangle): Prop :=
+  ∀ i : Fin 3, ‖p.rotM₂ (Q i)‖ > r + ε * √2
+
+-- FIXME: is this the cleanest way of getting convex hull from
+-- Finset? Probably not.
+noncomputable
+def shape_of (S : Finset ℝ³) : Shape where
+  size := S.card
+  vertices i := (S.equivFin.symm i).1
+
+-- TODO: Somehow separate out the "local theorem precondition"
+-- predicate in a way that is suitable for the computational step's
+-- tree check.
 theorem local_theorem (P Q : Triangle)
     (cong_tri : P.Congruent Q)
-    (bP : Finset ℝ³) [Nonempty bP]
-    (radius_one : polyhedron_radius bP = 1)
-    (ε : ℝ) (hε : ε > 0)
+    (poly : Finset ℝ³) [Nonempty poly]
+    (hP : P ∈ poly) (hQ : Q ∈ poly)
+    (radius_one : polyhedron_radius poly = 1)
     (p : LooseViewPose)
-    (az₁ : P.Az p.vecX₁ ε) (az₂ : Q.Az p.vecX₂ ε)
-    : True := by -- FIXME not all premises here yet
+    (ε δ r : ℝ) (hε : ε > 0) (hr : r > 0)
+    (hr : BoundR r ε p Q)
+    (hδ : BoundDelta δ p P Q)
+    (ae₁ : P.Aε p.vecX₁ ε) (ae₂ : Q.Aε p.vecX₂ ε)
+    (span₁ : P.Spanning p.θ₁ p.φ₁ ε)
+    (span₂ : Q.Spanning p.θ₂ p.φ₂ ε)
+    (be : Q.Bε p ε δ r)
+    : ∃ q ∈ p.closed_ball ε, Shadows.IsRupert q (shape_of poly |>.hull) := by
   sorry
