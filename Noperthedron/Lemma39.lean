5542f5b2dd97d30558464d63bce5815fc9e57987
Jason Reed <jcreed@gmail.com>
2025-11-27 17:21:19 -0500

    feat: progress on [SY25] Lemma 39, filling out calc

--- Noperthedron/Lemma39.lean
+++ Noperthedron/Lemma39.lean
@@ -13,7 +13,15 @@ theorem norm_le_delta_sqrt_dims {m n : ℕ} {δ : ℝ} (A : Matrix (Fin m) (Fin 
   intro v
   suffices h : ‖(Matrix.toEuclideanLin A) v‖^2 ≤ (δ * √(↑m * ↑n) * ‖v‖)^2 from
     (sq_le_sq₀ (by positivity) (by positivity)).mp h
-  simp only [PiLp.norm_sq_eq_of_L2, Matrix.piLp_ofLp_toEuclideanLin, Matrix.toLin'_apply,
-    Real.norm_eq_abs, sq_abs, Nat.cast_nonneg, Real.sqrt_mul, mul_pow, Real.sq_sqrt]
+  simp only [Nat.cast_nonneg, Real.sqrt_mul, mul_pow, Real.sq_sqrt]
   ring_nf
-  sorry
+
+  calc ‖A.toEuclideanLin v‖^2
+    _ = ∑ i, A.mulVec v.ofLp i ^ 2 := by simp [PiLp.norm_sq_eq_of_L2]
+    _ = ∑ i, (∑ j, A i j * v j) ^ 2 := by simp [Matrix.mulVec, dotProduct]
+    _ ≤ ∑ i : Fin m, (∑ j, δ * |v j|) ^ 2 := by sorry
+    _ = m * (∑ j, δ * |v j|) ^ 2 := by sorry
+    _ = m * (δ * ∑ j, |v j|) ^ 2 := by sorry
+    _ = m * δ^2 * (∑ j, |v j|) ^ 2 := by sorry
+    _ ≤ m * δ^2 * (∑ j : Fin n, ‖v‖) ^ 2 := by sorry
+    _ ≤  δ ^ 2 * m * n * ‖v‖^2 := by  simp [PiLp.norm_sq_eq_of_L2]; sorry
