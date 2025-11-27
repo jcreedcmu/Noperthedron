e308a5a2a1bb7af118ed41b4b93bfad8fb9e1abe
Jason Reed <jcreed@gmail.com>
2025-11-27 17:59:12 -0500

    extract some lemmas

--- Noperthedron/Lemma39.lean
+++ Noperthedron/Lemma39.lean
@@ -4,8 +4,18 @@ namespace RationalApprox
 
 noncomputable section
 
--- example (a : ℝ) (m : ℕ) (b : Fin m → ℝ)  : ∑ i : Fin m, a * b i = a * ∑ i : Fin m, b i := by exact
---   Eq.symm (Finset.mul_sum Finset.univ b a)
+-- Use the triangle inequality
+theorem sum_mul_sq_le_sum_mul_abs_sq {n : ℕ} (v w : Fin n → ℝ)
+    (δ : ℝ) (hle : ∀ j, |w j| ≤ δ) :
+    (∑ j, w j * v j)^2 ≤ (∑ j, δ * |v j|)^2 := by
+  sorry
+
+-- Use Cauchy-Schwartz
+theorem sum_abs_sq_le_sum_abs_sq_mul {n : ℕ} (v : Fin n → ℝ) :
+    (∑ j, |v j|) ^ 2 ≤ (∑ j, |v j| ^ 2) * n := by
+  sorry
+
+example (a : ℝ) : a ≤ a := by simp_all only [le_refl]
 
 theorem norm_le_delta_sqrt_dims {m n : ℕ} {δ : ℝ} (A : Matrix (Fin m) (Fin n) ℝ)
     (hδ : 0 < δ) (hle : ∀ i j, |A i j| ≤ δ) :
@@ -14,7 +24,7 @@ theorem norm_le_delta_sqrt_dims {m n : ℕ} {δ : ℝ} (A : Matrix (Fin m) (Fin 
   refine csInf_le (by use 0; intro b hb; exact hb.1) ?_
   refine ⟨by positivity, ?_⟩
   intro v
-  suffices h : ‖(Matrix.toEuclideanLin A) v‖^2 ≤ (δ * √(↑m * ↑n) * ‖v‖)^2 from
+  suffices h : ‖(Matrix.toEuclideanLin A) v‖^2 ≤ (δ * √(m * n) * ‖v‖)^2 from
     (sq_le_sq₀ (by positivity) (by positivity)).mp h
   simp only [Nat.cast_nonneg, Real.sqrt_mul, mul_pow, Real.sq_sqrt]
   ring_nf
@@ -22,11 +32,12 @@ theorem norm_le_delta_sqrt_dims {m n : ℕ} {δ : ℝ} (A : Matrix (Fin m) (Fin 
   calc ‖A.toEuclideanLin v‖^2
     _ = ∑ i, A.mulVec v.ofLp i ^ 2 := by simp [PiLp.norm_sq_eq_of_L2]
     _ = ∑ i, (∑ j, A i j * v j) ^ 2 := by simp [Matrix.mulVec, dotProduct]
-    _ ≤ ∑ i : Fin m, (∑ j, δ * |v j|) ^ 2 := by sorry -- use triangle inequality and hle here
+    _ ≤ ∑ i : Fin m, (∑ j, δ * |v j|) ^ 2 :=
+        Finset.sum_le_sum (fun i _ => sum_mul_sq_le_sum_mul_abs_sq v.ofLp (A i) δ (hle i))
     _ = m * (∑ j, δ * |v j|) ^ 2 := by
         simp only [Finset.sum_const, Finset.card_univ, Fintype.card_fin, nsmul_eq_mul]
     _ = m * (δ * (∑ j, |v j|)) ^ 2 := by rw [← Finset.mul_sum]
     _ = m * δ^2 * (∑ j, |v j|) ^ 2 := by rw [mul_pow]; ring_nf
-    _ ≤ m * δ^2 * (∑ j, |v j| ^ 2) * n := by sorry -- use Cauchy-Schwartz here
+    _ ≤ m * δ^2 * (∑ j, |v j| ^ 2) * n := by grw [sum_abs_sq_le_sum_abs_sq_mul v]; ring_nf; simp only [le_refl]
     _ = m * δ^2 * (∑ j, (v j) ^ 2) * n := by conv in (∑ j, _) => rhs; intro j; rw [sq_abs]
     _ = δ ^ 2 * m * n * ‖v‖^2 := by simp [PiLp.norm_sq_eq_of_L2]; ring_nf
