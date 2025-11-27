44e0a7d1dfdc09cfd6cd3e06967842a41e0a0ab1
Jason Reed <jcreed@gmail.com>
2025-11-27 17:34:29 -0500

    feat: progress on [SY25] Lemma 39, only inequalities remain

--- Noperthedron/Lemma39.lean
+++ Noperthedron/Lemma39.lean
@@ -4,6 +4,9 @@ namespace RationalApprox
 
 noncomputable section
 
+-- example (a : ℝ) (m : ℕ) (b : Fin m → ℝ)  : ∑ i : Fin m, a * b i = a * ∑ i : Fin m, b i := by exact
+--   Eq.symm (Finset.mul_sum Finset.univ b a)
+
 theorem norm_le_delta_sqrt_dims {m n : ℕ} {δ : ℝ} (A : Matrix (Fin m) (Fin n) ℝ)
     (hδ : 0 < δ) (hle : ∀ i j, |A i j| ≤ δ) :
     ‖A.toEuclideanLin.toContinuousLinearMap‖ ≤ δ * √(m * n) := by
@@ -19,9 +22,11 @@ theorem norm_le_delta_sqrt_dims {m n : ℕ} {δ : ℝ} (A : Matrix (Fin m) (Fin 
   calc ‖A.toEuclideanLin v‖^2
     _ = ∑ i, A.mulVec v.ofLp i ^ 2 := by simp [PiLp.norm_sq_eq_of_L2]
     _ = ∑ i, (∑ j, A i j * v j) ^ 2 := by simp [Matrix.mulVec, dotProduct]
-    _ ≤ ∑ i : Fin m, (∑ j, δ * |v j|) ^ 2 := by sorry
-    _ = m * (∑ j, δ * |v j|) ^ 2 := by sorry
-    _ = m * (δ * ∑ j, |v j|) ^ 2 := by sorry
-    _ = m * δ^2 * (∑ j, |v j|) ^ 2 := by sorry
-    _ ≤ m * δ^2 * (∑ j : Fin n, ‖v‖) ^ 2 := by sorry
-    _ ≤  δ ^ 2 * m * n * ‖v‖^2 := by  simp [PiLp.norm_sq_eq_of_L2]; sorry
+    _ ≤ ∑ i : Fin m, (∑ j, δ * |v j|) ^ 2 := by sorry -- use triangle inequality and hle here
+    _ = m * (∑ j, δ * |v j|) ^ 2 := by
+        simp only [Finset.sum_const, Finset.card_univ, Fintype.card_fin, nsmul_eq_mul]
+    _ = m * (δ * (∑ j, |v j|)) ^ 2 := by rw [← Finset.mul_sum]
+    _ = m * δ^2 * (∑ j, |v j|) ^ 2 := by rw [mul_pow]; ring_nf
+    _ ≤ m * δ^2 * (∑ j, |v j| ^ 2) * n := by sorry -- use Cauchy-Schwartz here
+    _ = m * δ^2 * (∑ j, (v j) ^ 2) * n := by conv in (∑ j, _) => rhs; intro j; rw [sq_abs]
+    _ = δ ^ 2 * m * n * ‖v‖^2 := by simp [PiLp.norm_sq_eq_of_L2]; ring_nf
