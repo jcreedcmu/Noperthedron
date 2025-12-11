34eed3ae5977b43a4a50539e3b86f3aaeb0e0011
David Renshaw <dwrenshaw@gmail.com>
2025-12-11 18:53:39 -0500

    [RationalApprox] golf cos_approx_sum

--- Noperthedron/RationalApprox.lean
+++ Noperthedron/RationalApprox.lean
@@ -68,6 +68,17 @@ theorem finset_sum_range_even_odd {n : ℕ} {f : ℕ → ℝ}
     nth_rw 2 [Finset.sum_range_succ, Finset.sum_range_succ]
     ring_nf
 
+theorem finset_sum_range_even_odd' {n : ℕ} {f : ℕ → ℝ}
+    : ∑ i ∈ Finset.range (2 * n), f i =
+      ∑ i ∈ Finset.range n, f (2 * i + 1) + ∑ i ∈ Finset.range n, f (2 * i) := by
+  induction n with
+  | zero => simp
+  | succ n ih =>
+    rw [show 2 * (n + 1) = 2 * n + 1 + 1 by ring]
+    rw [Finset.sum_range_succ, Finset.sum_range_succ, ih]
+    rw [Finset.sum_range_succ, Finset.sum_range_succ]
+    ring_nf
+
 theorem taylorWithinEval_sin {n : ℕ} {x : ℝ} (hx' : 0 < x) :
     taylorWithinEval Real.sin (2 * n) (Set.Icc 0 x) 0 x =
        ∑ i ∈ Finset.range n, (-1) ^ i * (x ^ (2 * i + 1) / ↑(2 * i + 1)!) := by
@@ -108,93 +119,42 @@ theorem sin_approx_aux (x : ℝ) (n : ℕ) :
   simp only [abs_div, abs_mul, abs_pow, abs_neg, abs_one, one_pow, one_mul, Nat.abs_cast, fieldLe]
   exact Real.abs_cos_le_one c
 
-set_option maxHeartbeats 500000 in
+theorem taylorWithinEval_cos {n : ℕ} {x : ℝ} (hx' : 0 < x) :
+    taylorWithinEval Real.cos (2 * n + 1) (Set.Icc 0 x) 0 x =
+       ∑ i ∈ Finset.range (n + 1), (-1) ^ i * x ^ (2 * i) / ↑(2 * i)! := by
+  have h_poly_eval : taylorWithinEval Real.cos (2 * n + 1) (Set.Icc 0 x) 0 x =
+       ∑ i ∈ Finset.range (2 * n + 1 + 1), (iteratedDeriv i Real.cos 0) * x ^ i / (i ! : ℝ) := by
+    rw [taylor_within_apply]
+    congr; ext i
+    rw [iteratedDerivWithin_eq_iteratedDeriv
+          (uniqueDiffOn_Icc hx') Real.contDiff_cos.contDiffAt (Set.left_mem_Icc.mpr hx'.le)]
+    simp [field]
+  rw [show 2 * n + 1 + 1 = 2 * (n + 1) by ring] at h_poly_eval
+  rw [finset_sum_range_even_odd'] at h_poly_eval
+  rw [h_poly_eval]
+  simp [mul_div_assoc]
+
 /--
 The difference between cos(x) and its Taylor polynomial of degree 2n-2 is bounded by |x|^(2n)/(2n)!.
 -/
 theorem Real.cos_approx_sum (x : ℝ) (n : ℕ) :
     |Real.cos x - ∑ i ∈ Finset.range n, (-1)^i * x^(2*i) / (2*i)!| ≤ |x|^(2*n) / (2*n)! := by
-  induction n generalizing x with
+  cases n with
   | zero => simpa using Real.abs_cos_le_one x
-  | succ n ih =>
-    -- Apply the induction hypothesis to rewrite the sum.
-    have h_rewrite :
-          |Real.cos x - ∑ i ∈ Finset.range (n + 1), (-1 : ℝ) ^ i * x ^ (2 * i) / (2 * i)!| =
-          |∫ t in (0 : ℝ)..x, (Real.sin t - ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * t ^ (2 * i + 1) /
-           (2 * i + 1)!)| := by
-      rw [intervalIntegral.integral_sub]
-      · -- The integral of the series up to n terms is the same as the sum of the series up to n terms.
-        simp only [Finset.sum_range_succ', pow_zero, mul_zero, mul_one, Nat.factorial_zero,
-          Nat.cast_one, ne_eq, one_ne_zero, not_false_eq_true, div_self, integral_sin, Real.cos_zero]
-        have h_integral :
-              ∫ x in (0 : ℝ)..x, ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * x ^ (2 * i + 1) / (2 * i + 1)! =
-              ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * x ^ (2 * i + 2) / (2 * i + 2)! := by
-          rw [intervalIntegral.integral_finset_sum]
-          · simp_all only [intervalIntegral.integral_div, intervalIntegral.integral_const_mul,
-              integral_pow, ne_eq, Nat.add_eq_zero_iff, mul_eq_zero, OfNat.ofNat_ne_zero, false_or,
-              one_ne_zero, and_false, and_self, not_false_eq_true, zero_pow, sub_zero, Nat.cast_add,
-              Nat.cast_mul, Nat.cast_ofNat, Nat.cast_one]
-            field_simp
-            exact Finset.sum_congr rfl fun _ _ => by push_cast [ Nat.factorial_succ ]; ring
-          · aesop
-        rw [h_integral]
-        simp only [pow_succ', neg_mul, one_mul, Nat.mul_succ, div_eq_mul_inv, Finset.sum_neg_distrib]
-        rw [← abs_neg]
-        ring_nf
-      · norm_num [Finset.sum_range_succ']
-      · exact Continuous.intervalIntegrable ( by fun_prop ) _ _
-    -- Apply the induction hypothesis to the integral.
-    have h_ind : ∀ t ∈ Set.Icc (0 : ℝ) |x|,
-        |Real.sin t - ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * t ^ (2 * i + 1) / (2 * i + 1)!| ≤
-        |t| ^ (2 * n + 1) / (2 * n + 1)! := by
-      intro t ht
-      have h_ind : |Real.sin t - ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * t ^ (2 * i + 1) / (2 * i + 1)!| = |∫ u in (0 : ℝ)..t, (Real.cos u - ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * u ^ (2 * i) / (2 * i)!)| := by
-        rw [intervalIntegral.integral_sub (by simp)]
-        · rw [intervalIntegral.integral_finset_sum (by simp)]
-          norm_num [div_eq_mul_inv, mul_assoc, mul_comm, mul_left_comm, Nat.factorial_succ]
-        · exact Continuous.intervalIntegrable (by fun_prop) _ _
-      -- Apply the induction hypothesis to the integral inside the absolute value.
-      have h_ind : |∫ u in (0 : ℝ)..t, (Real.cos u - ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * u ^ (2 * i) / (2 * i)!)| ≤ ∫ u in (0 : ℝ)..t, |u| ^ (2 * n) / (2 * n)! := by
-        rw [intervalIntegral.integral_of_le ht.1, intervalIntegral.integral_of_le ht.1]
-        refine le_trans (MeasureTheory.norm_integral_le_integral_norm (_ : ℝ → ℝ)) ( MeasureTheory.integral_mono_of_nonneg ?_ ?_ ?_)
-        · exact Filter.Eventually.of_forall fun _ => norm_nonneg _
-        · exact Continuous.integrableOn_Ioc ( by fun_prop )
-        · filter_upwards [ MeasureTheory.ae_restrict_mem measurableSet_Ioc ] with u hu using ih u
-      convert h_ind using 1
-      rw [intervalIntegral.integral_congr fun u hu => by rw [abs_of_nonneg]; linarith only [ Set.mem_Icc.mp ( by simpa [ ht.1 ] using hu )]]
-      simp only [mul_comm, pow_succ, Nat.factorial_succ, Nat.cast_mul, Nat.cast_add, Nat.cast_ofNat,
-        Nat.cast_one, div_eq_mul_inv, mul_inv_rev, mul_assoc, intervalIntegral.integral_mul_const,
-        integral_pow, zero_mul, sub_zero, mul_left_comm]
-      rw [abs_of_nonneg ht.1]
-    -- Apply the induction hypothesis to bound the integral.
-    have h_integral_bound : |∫ t in (0 : ℝ)..x, (Real.sin t - ∑ i ∈ Finset.range n, (-1 : ℝ) ^ i * t ^ (2 * i + 1) / (2 * i + 1)!)| ≤ ∫ t in (0 : ℝ)..|x|, |t| ^ (2 * n + 1) / (2 * n + 1)! := by
-      obtain hx | hx := abs_cases x <;> simp_all [intervalIntegral]
-      · rw [abs_of_nonneg hx]
-        refine le_trans (MeasureTheory.norm_integral_le_integral_norm (_ : ℝ → ℝ)) ?_
-        refine MeasureTheory.integral_mono_of_nonneg ?_ ?_ ?_
-        · exact Filter.Eventually.of_forall fun _ => norm_nonneg _;
-        · exact Continuous.integrableOn_Ioc (by fun_prop)
-        · filter_upwards [ MeasureTheory.ae_restrict_mem measurableSet_Ioc ] with t ht using h_ind t ht.1.le ht.2;
-      · refine le_trans (MeasureTheory.norm_integral_le_integral_norm (_ : ℝ → ℝ)) ?_
-        refine le_trans (MeasureTheory.integral_mono_of_nonneg
-                         (g := fun (t:ℝ) ↦ |t| ^ (2 * n + 1) / (2 * n + 1)!) ?_ ?_ ?_) ?_
-        · exact Filter.Eventually.of_forall fun _ => norm_nonneg _
-        · exact Continuous.integrableOn_Ioc (by fun_prop)
-        · filter_upwards [ MeasureTheory.ae_restrict_mem measurableSet_Ioc ] with t ht
-          convert h_ind (-t) (by grind) (by grind) using 1 <;> norm_num [abs_neg, abs_of_nonpos, ht.1.le, ht.2]
-          simp only [pow_succ', even_two, Even.mul_right, Even.neg_pow, neg_mul, mul_neg, neg_div,
-            Finset.sum_neg_distrib, sub_neg_eq_add]
-          rw [ neg_add_eq_sub, abs_sub_comm ]
-        · rw [← intervalIntegral.integral_of_le hx.1, ← intervalIntegral.integral_of_le (abs_nonneg x)]
-          convert intervalIntegral.integral_comp_neg _ |> le_of_eq using 2 <;> norm_num [abs_of_nonpos hx.1]
-    refine h_rewrite ▸ h_integral_bound.trans ?_
-    rw [intervalIntegral.integral_congr fun t ht => by rw [abs_of_nonneg]; aesop]
-    simp only [pow_succ, Nat.factorial_succ, Nat.cast_mul, Nat.cast_add, Nat.cast_ofNat,
-      Nat.cast_one, intervalIntegral.integral_div, Nat.mul_succ]
-    simp only [← pow_succ, integral_pow, ne_eq, Nat.add_eq_zero_iff, mul_eq_zero,
-      OfNat.ofNat_ne_zero, false_or, one_ne_zero, and_false, and_self, not_false_eq_true, zero_pow,
-      sub_zero, Nat.cast_add, Nat.cast_mul, Nat.cast_ofNat, Nat.cast_one];
-    rw [div_div]
+  | succ n =>
+    rw [le_div_iff₀ (by positivity)]
+    wlog hx : 0 < x with H
+    · obtain rfl | hx := eq_or_lt_of_not_gt hx
+      · simp [Finset.sum_range_succ']
+      · specialize H (-x) n (by linarith)
+        simpa using H
+    obtain ⟨c, hc₁, hc₂⟩ :=
+      taylor_mean_remainder_lagrange_iteratedDeriv (n := 2 * n + 1) hx Real.contDiff_cos.contDiffOn
+    simp only [taylorWithinEval_cos hx, sub_zero] at hc₂
+    simp only [hc₂, show 2 * n + 1 + 1 = 2 * (n + 1) by linarith]
+    simp only [Real.iteratedDeriv_even_cos, Pi.mul_apply, Pi.pow_apply, Pi.neg_apply, Pi.one_apply,
+      abs_div, abs_mul, abs_pow, abs_neg, abs_one, one_pow, one_mul, Nat.abs_cast, fieldLe]
+    exact Real.abs_cos_le_one c
 
 theorem cos_psum_eq_real_sum (n : ℕ) (x : ℚ) : (RationalApprox.cos_psum n x : ℝ) = ∑ i ∈ Finset.range n, (-1)^i * (x : ℝ)^(2*i) / (2*i)! := by
   simp only [cos_psum, Rat.cast_sum, Rat.cast_mul, Rat.cast_pow, Rat.cast_neg, Rat.cast_one,
