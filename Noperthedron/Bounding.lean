ec98b29f8b0a06587713154e2698ead3ba496046
David Renshaw <dwrenshaw@gmail.com>
2025-12-18 12:29:19 -0500

    [Bounding] inline the subst

--- Noperthedron/Bounding.lean
+++ Noperthedron/Bounding.lean
@@ -148,10 +148,9 @@ theorem norm_rotR_sub_rotR_lt {ε α α_ : ℝ} (hε : 0 < ε) (hα : |α - α_|
   have h_sin_bound : |Real.sin ((α - α_) / 2)| ≤ |α - α_| / 2 := by
     apply le_trans Real.abs_sin_le_abs
     rw [abs_div, abs_two]
-  obtain h | h := lt_or_eq_of_le hα
+  obtain h | rfl := lt_or_eq_of_le hα
   · exact lt_of_le_of_lt (h_op_norm α α_) (by linarith)
-  · subst h
-    simp_all only [rotR, rotR_mat, AddChar.coe_mk, abs_pos, ne_eq, le_refl, gt_iff_lt]
+  · simp_all only [rotR, rotR_mat, AddChar.coe_mk, abs_pos, ne_eq, le_refl, gt_iff_lt]
     -- Since $|α - α_| > 0$, we have $|Real.sin ((α - α_) / 2)| < |α - α_| / 2$.
     have h_sin_lt : |Real.sin ((α - α_) / 2)| < |α - α_| / 2 := by
       have h_sin_lt : ∀ x : ℝ, 0 < x → x < Real.pi → |Real.sin x| < x := by
