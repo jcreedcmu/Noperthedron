753594d8cdbef5376bb0ca85c6bdee7905e30138
David Renshaw <dwrenshaw@gmail.com>
2025-12-28 15:07:43 -0500

    oops forgot to check in EuclideanSpaceNotation.lean

---
+++ Noperthedron/EuclideanSpaceNotation.lean
@@ -0,0 +1,6 @@
+import Mathlib.Analysis.InnerProductSpace.PiL2
+
+notation "ℝ³" => EuclideanSpace ℝ (Fin 3)
+notation "ℝ²" => EuclideanSpace ℝ (Fin 2)
+
+notation "Euc(" n:arg ")" => EuclideanSpace ℝ (Fin n)
