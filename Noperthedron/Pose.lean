02376a1057f63977683e0da92a8b8118e242cb41
Jason Reed <jcreed@gmail.com>
2025-12-30 12:42:06 -0500

    [Pose] try generalizing pose equivalence to negation

--- Noperthedron/Pose.lean
+++ Noperthedron/Pose.lean
@@ -46,8 +46,9 @@ def inner (p : Pose) : ℝ³ →ᵃ[ℝ] ℝ² := innerProj p
 noncomputable
 def outer (p : Pose) : ℝ³ →ᵃ[ℝ] ℝ² := outerProj p
 
-def equiv (p1 p2 : Pose) : Prop :=
-  p1.inner = p2.inner ∧ p1.outer = p2.outer
+inductive equiv (p1 p2 : Pose) : Prop where
+  | on_the_nose : p1.inner = p2.inner ∧ p1.outer = p2.outer → equiv p1 p2
+  | off_by_neg : p1.inner = -p2.inner ∧ p1.outer = -p2.outer → equiv p1 p2
 
 def innerParams (p : Pose) : ℝ³ := WithLp.toLp 2 ![p.α, p.θ₁, p.φ₁]
 
@@ -103,36 +104,52 @@ lemma outer_shadow_eq_M (p : Pose) (S : Set ℝ³) :
     outerShadow p S = p.rotM₂ '' S := by
   sorry
 
-lemma poselike_inner_eq_proj_inner {v : ℝ³} (p : Pose) :
-    proj_xyL (PoseLike.inner p v) = p.inner v := by
-  ext i; fin_cases i <;>
+lemma poselike_inner_eq_proj_inner (p : Pose) :
+    proj_xyL ∘ PoseLike.inner p = p.inner := by
+  ext v i; fin_cases i <;>
     simp [Matrix.vecHead, PoseLike.inner, Pose.inner, innerProj]
 
-lemma poselike_outer_eq_proj_outer {v : ℝ³} (p : Pose) :
-    proj_xyL (PoseLike.outer p v) = p.outer v := by
-  ext i; fin_cases i <;>
+lemma poselike_outer_eq_proj_outer (p : Pose) :
+    proj_xyL ∘ PoseLike.outer p = p.outer := by
+  ext v i; fin_cases i <;>
     simp [Matrix.vecHead, PoseLike.outer, Pose.outer, outerProj]
 
 lemma equiv_rupert_imp_rupert {p1 p2 : Pose} {S : Set ℝ³} (e : equiv p1 p2) (r : RupertPose p1 S) :
     RupertPose p2 S := by
-  simp only [RupertPose, innerShadow, outerShadow]
-  obtain ⟨e_inner, e_outer⟩ := e
-  conv =>
-    lhs; arg 1; arg 1; intro x; arg 1; intro v
-    rw [poselike_inner_eq_proj_inner p2, ← e_inner,
-      ← poselike_inner_eq_proj_inner p1]
-  conv =>
-    rhs; arg 1; arg 1; intro x; arg 1; intro v
-    rw [poselike_outer_eq_proj_outer p2, ← e_outer,
-      ← poselike_outer_eq_proj_outer p1]
-  exact r
+  match e with
+  | .on_the_nose e =>
+    simp only [RupertPose, innerShadow, outerShadow]
+    obtain ⟨e_inner, e_outer⟩ := e
+    calc
+      closure ((proj_xyL ∘ PoseLike.inner p2) '' S)
+      _ = closure (p2.inner '' S) := by rw [poselike_inner_eq_proj_inner p2]
+      _ = closure (p1.inner '' S) := by rw [e_inner]
+      _ ⊆ interior (p1.outer '' S) := r
+      _ = interior (p2.outer '' S) := by rw [e_outer]
+      _ = interior ((proj_xyL ∘ PoseLike.outer p2) '' S) := by rw [poselike_outer_eq_proj_outer p2]
+  | .off_by_neg e =>
+    simp only [RupertPose, innerShadow, outerShadow]
+    obtain ⟨e_inner, e_outer⟩ := e
+    calc
+      closure ((proj_xyL ∘ PoseLike.inner p2) '' S)
+      _ = closure (p2.inner '' S) := by rw [poselike_inner_eq_proj_inner p2]
+      _ = closure (-p1.inner '' S) := by
+        rw [e_inner]; simp only [AffineMap.coe_neg, Pi.neg_apply];
+        congr; ext; simp only [Set.mem_neg, Set.mem_image, neg_inj]
+      _ = -closure (p1.inner '' S) := Eq.symm (neg_closure (p1.inner '' S))
+      _ ⊆ -(interior (p1.outer '' S)) := by rw [Set.neg_subset_neg]; exact r
+      _ = interior (-(p1.outer '' S)) := (Homeomorph.neg ℝ²).preimage_interior (p1.outer '' S)
+      _ = interior (-(-p2.outer) '' S) := by rw [e_outer]
+      _ = interior (p2.outer '' S) := by congr; ext; simp
+      _ = interior ((proj_xyL ∘ PoseLike.outer p2) '' S) := by rw [poselike_outer_eq_proj_outer p2]
 
 lemma matrix_eq_imp_pose_equiv {p q : Pose} (re : p.rotR = q.rotR)
     (rm1 : p.rotM₁ = q.rotM₁) (rm2 : p.rotM₂ = q.rotM₂) : equiv p q := by
+  refine equiv.on_the_nose ?_
   constructor
   · simp only [inner, innerProj, PoseLike.inner]
     ext1 v
-    simp only [ AffineMap.coe_comp,
+    simp only [AffineMap.coe_comp,
       LinearMap.coe_toAffineMap, ContinuousLinearMap.coe_coe, Function.comp_apply]
     change (proj_xyL ∘ rotRM p.θ₁ p.φ₁ p.α) v = (proj_xyL ∘ rotRM q.θ₁ q.φ₁ q.α) v
     rw [projxy_rotRM_eq_rotprojRM, projxy_rotRM_eq_rotprojRM]
@@ -141,12 +158,36 @@ lemma matrix_eq_imp_pose_equiv {p q : Pose} (re : p.rotR = q.rotR)
     rw [re, rm1]
   · simp only [outer, outerProj, PoseLike.outer]
     ext1 v
-    simp only [ AffineMap.coe_comp,
+    simp only [AffineMap.coe_comp,
       LinearMap.coe_toAffineMap, ContinuousLinearMap.coe_coe, Function.comp_apply]
     change (proj_xyL ∘ rotRM p.θ₂ p.φ₂ 0) v = (proj_xyL ∘ rotRM q.θ₂ q.φ₂ 0) v
     rw [projxy_rotRM_eq_rotprojRM, projxy_rotRM_eq_rotprojRM]
     simp only [rotprojRM, AddChar.map_zero_eq_one]
     change (p.rotM₂) v = (q.rotM₂) v
+    rw [rm2]
+
+lemma matrix_neg_imp_pose_equiv {p q : Pose} (re : p.rotR = q.rotR)
+    (rm1 : p.rotM₁ = -q.rotM₁) (rm2 : p.rotM₂ = -q.rotM₂) : equiv p q := by
+  refine equiv.off_by_neg ?_
+  constructor
+  · simp only [inner, innerProj, PoseLike.inner]
+    ext1 v
+    simp only [AffineMap.coe_comp,
+      LinearMap.coe_toAffineMap, ContinuousLinearMap.coe_coe, Function.comp_apply]
+    change (proj_xyL ∘ rotRM p.θ₁ p.φ₁ p.α) v = (-proj_xyL ∘ rotRM q.θ₁ q.φ₁ q.α) v
+    rw [projxy_rotRM_eq_rotprojRM, projxy_rotRM_eq_rotprojRM]
+    simp only [rotprojRM]
+    change (p.rotR ∘ p.rotM₁) v = (-q.rotR ∘ q.rotM₁) v
+    rw [re, rm1]
+    simp
+  · simp only [outer, outerProj, PoseLike.outer]
+    ext1 v
+    simp only [AffineMap.coe_comp,
+      LinearMap.coe_toAffineMap, ContinuousLinearMap.coe_coe, Function.comp_apply]
+    change (proj_xyL ∘ rotRM p.θ₂ p.φ₂ 0) v = (-proj_xyL ∘ rotRM q.θ₂ q.φ₂ 0) v
+    rw [projxy_rotRM_eq_rotprojRM, projxy_rotRM_eq_rotprojRM]
+    simp only [rotprojRM, AddChar.map_zero_eq_one]
+    change (p.rotM₂) v = (-q.rotM₂) v
     rw [rm2]
 
 end Pose
