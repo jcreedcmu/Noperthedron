355452150a1250ec50f50b3187c3a0b01137d366
Jason Reed <jcreed@gmail.com>
2025-12-31 15:05:56 -0500

    [ConvertPose] refactoring to try to eliminate viewpose

--- Noperthedron/ConvertPose.lean
+++ Noperthedron/ConvertPose.lean
@@ -1,25 +1,26 @@
 import Noperthedron.MatrixPose
 import Noperthedron.ViewPose
+import Noperthedron.Pose
 
-def poseOfViewPose (v : ViewPose) : MatrixPose where
+def Pose.matrixPoseOfPose (p : Pose) : MatrixPose where
   innerRot := sorry -- TODO(easy)
   outerRot := sorry -- TODO(easy)
   innerOffset := 0
 
-theorem converted_pose_inner_shadow_eq (v : ViewPose) (S : Set ℝ³) :
-    innerShadow v S = innerShadow (poseOfViewPose v) S := by
+theorem converted_pose_inner_shadow_eq (v : Pose) (S : Set ℝ³) :
+    innerShadow v S = innerShadow (v.matrixPoseOfPose) S := by
   sorry -- TODO(easy)
 
-theorem converted_pose_outer_shadow_eq (v : ViewPose) (S : Set ℝ³) :
-    outerShadow v S = outerShadow (poseOfViewPose v) S := by
+theorem converted_pose_outer_shadow_eq (v : Pose) (S : Set ℝ³) :
+    outerShadow v S = outerShadow (v.matrixPoseOfPose) S := by
   sorry -- TODO(easy)
 
-theorem converted_pose_rupert_iff (v : ViewPose) (S : Set ℝ³) :
-    RupertPose v S ↔ RupertPose (poseOfViewPose v) S := by
+theorem converted_pose_rupert_iff (v : Pose) (S : Set ℝ³) :
+    RupertPose v S ↔ RupertPose (v.matrixPoseOfPose) S := by
   constructor; all_goals
   · unfold RupertPose
     rw [converted_pose_inner_shadow_eq, converted_pose_outer_shadow_eq]
     exact id
 
-theorem view_pose_of_pose (p : MatrixPose) : ∃ v : ViewPose, poseOfViewPose v = p.zeroOffset := by
+theorem view_pose_of_pose (p : MatrixPose) : ∃ pp : Pose, pp.matrixPoseOfPose = p.zeroOffset := by
   sorry -- TODO(easy)
