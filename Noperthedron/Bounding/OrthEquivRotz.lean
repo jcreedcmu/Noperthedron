f7320df16f24a97cead983ac7ba812afebb00224
Jason Reed <jcreed@gmail.com>
2026-01-02 18:44:49 -0500

    [OrthEquivRotz] finish proof of last lemmas in this file
    Co-authored-by: Aristotle Harmonic <aristotle-harmonic@harmonic.fun>

--- Noperthedron/Bounding/OrthEquivRotz.lean
+++ Noperthedron/Bounding/OrthEquivRotz.lean
@@ -246,32 +246,27 @@ lemma to_euc_mul {a b c : ℕ}
     (u : Euc(a) →ₗ[ℝ] Euc(b)) (v : Euc(b) →ₗ[ℝ] Euc(c)) :
     Matrix.toEuclideanLin.symm v * Matrix.toEuclideanLin.symm u =
     Matrix.toEuclideanLin.symm (v ∘ₗ u) := by
-  sorry
+  refine LinearEquiv.injective Matrix.toEuclideanLin ?_
+  have (uu : Matrix (Fin b) (Fin a) ℝ) (vv : Matrix (Fin c) (Fin b) ℝ) :
+     Matrix.toEuclideanLin (vv * uu) = Matrix.toEuclideanLin vv ∘ₗ Matrix.toEuclideanLin uu := by
+    ext x
+    simp only [Matrix.toEuclideanLin_apply, LinearMap.coe_comp, Function.comp_apply,
+      Matrix.mulVec_mulVec]
+  rw [this]
+  simp
 
-lemma to_euc_id {n : ℕ} : LinearMap.id (M := Euc(n)) = Matrix.toEuclideanLin 1 := by
-  sorry
-
-noncomputable
-instance {n : ℕ} (u : Euc(n) ≃ₗ[ℝ] Euc(n)) :
-    Invertible (Matrix.toEuclideanLin.symm u.toLinearMap) where
-  invOf := Matrix.toEuclideanLin.symm u.symm.toLinearMap
-  invOf_mul_self := by
-    simp only [to_euc_mul, LinearEquiv.comp_coe, LinearEquiv.self_trans_symm, LinearEquiv.refl_toLinearMap]
-    exact (LinearEquiv.symm_apply_eq Matrix.toEuclideanLin).mpr to_euc_id
-  mul_invOf_self := by
-    simp only [to_euc_mul, LinearEquiv.comp_coe, LinearEquiv.symm_trans_self, LinearEquiv.refl_toLinearMap]
-    refine (LinearEquiv.symm_apply_eq Matrix.toEuclideanLin).mpr to_euc_id
+lemma to_euc_one {n : ℕ} : Matrix.toEuclideanLin.symm (LinearMap.id (M := Euc(n))) = 1 := by
+  ext i j
+  rw [Matrix.toEuclideanLin]
+  simp only [LinearEquiv.trans_symm, Matrix.toLin'_symm, LinearEquiv.trans_apply, LinearMap.toMatrix'_apply]
+  rfl
 
 lemma inv_euclidean_eq_euclidean_symm (u : Euc(3) ≃ₗ[ℝ] Euc(3)) :
     (Matrix.toEuclideanLin.symm u.toLinearMap)⁻¹ = Matrix.toEuclideanLin.symm u.symm.toLinearMap := by
-  let inst : Invertible (Matrix.toEuclideanLin.symm u.toLinearMap) := inferInstance
-  have h : 1 = (Matrix.toEuclideanLin.symm u.toLinearMap) * (Matrix.toEuclideanLin.symm u.symm.toLinearMap)  :=
-    inst.mul_invOf_self |>.symm
-  have : (Matrix.toEuclideanLin.symm u.toLinearMap)⁻¹ * 1 =
-      (Matrix.toEuclideanLin.symm u.toLinearMap)⁻¹ * ((Matrix.toEuclideanLin.symm u.toLinearMap) * (Matrix.toEuclideanLin.symm u.symm.toLinearMap))  := by
-    congr
-  simp at this
-  exact this
+  rw [ Matrix.inv_eq_right_inv ];
+  rw [ to_euc_mul u.symm.toLinearMap u.toLinearMap ];
+  simp only [LinearEquiv.comp_coe, LinearEquiv.symm_trans_self, LinearEquiv.refl_toLinearMap]
+  exact to_euc_one
 
 lemma euclidean_linear_equiv_inverse (v : ℝ³) (u : Euc(3) ≃ₗ[ℝ] Euc(3)) (U : Matrix (Fin 3) (Fin 3) ℝ)
     (hu : U = Matrix.toEuclideanLin.symm u.toLinearMap) :
