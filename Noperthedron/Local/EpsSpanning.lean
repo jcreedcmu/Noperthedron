3ab3a06e4b4ec7571ef8b5125ded08e71118a11d
David Renshaw <dwrenshaw@gmail.com>
2026-01-12 11:23:20 -0500

    [EpsSpanning] finish proof of vecX_spanning
    Co-authored-by: Aristotle Harmonic <aristotle-harmonic@harmonic.fun>

--- Noperthedron/Local/EpsSpanning.lean
+++ Noperthedron/Local/EpsSpanning.lean
@@ -71,7 +71,32 @@ theorem vecX_spanning {ε θ θ_ φ φ_ : ℝ} (P : Triangle)
   -- both S and vecX θ φ are in the kernel of rotM θ φ
   -- therefore S = λ * vecX θ φ for some λ.
   have h₂ : ∃ lam : ℝ, S = lam • vecX θ φ := by
-    sorry
+    by_cases hSz : S = 0
+    · use 0; simpa
+    have h₆ : rotM θ φ S = 0 := by
+      unfold S
+      simpa
+   -- Since $S$ is orthogonal to the plane spanned by $rotM θ φ$, it must be parallel to $vecX θ φ$.
+    have hS_orthogonal₁ : ⟪vecX θ φ, S⟫ ≠ 0 := by
+      rw [ inner_add_right, inner_add_right, inner_smul_right, inner_smul_right, inner_smul_right ] ; nlinarith [ hX 0, hX 1, hX 2 ];
+    have hS_orthogonal : ⟪rotM θ φ S, rotM θ φ S⟫ = 0 := by
+      aesop;
+    have hS_orthogonal : ⟪S, S⟫ = ⟪vecX θ φ, S⟫ ^ 2 := by
+      have hS_orthogonal : ‖rotM θ φ S‖ ^ 2 = ‖S‖ ^ 2 - ⟪vecX θ φ, S⟫ ^ 2 := by
+        exact pythagoras S
+      simp_all [ inner_self_eq_norm_sq_to_K ]
+      linarith;
+    simp only [inner_self_eq_norm_sq_to_K, Real.ringHom_apply] at hS_orthogonal
+    rw [sq_eq_sq_iff_abs_eq_abs, abs_norm, ← Real.norm_eq_abs] at hS_orthogonal
+    rw [←one_mul ‖S‖, ← vecX_norm_one θ φ] at hS_orthogonal
+    symm at hS_orthogonal
+    have hXnz : vecX θ φ ≠ 0 := by
+      intro H
+      apply_fun (fun x ↦ ‖x‖) at H
+      simp [vecX_norm_one] at H
+    rw [norm_inner_eq_norm_iff hXnz hSz] at hS_orthogonal
+    obtain ⟨r, hr₁, hr₂⟩ := hS_orthogonal
+    use r
   obtain ⟨lam, hlam⟩ := h₂
   have h₄ : 0 < lam := by
     have h₃ : lam = ⟪vecX θ φ, lam • vecX θ φ⟫ := by
