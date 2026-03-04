a03943fc0ffa63e75ef6df2259e5728d654a46d9
Cameron Freer <cameronfreer@gmail.com>
2026-03-04 14:02:02 +0000

    Cache Witness build artifacts (.olean, .ilean, .c, .setup.json)
    Witness.lean builds successfully with all native_decide checks passing.
    The 13 remaining sorries are all oracle trust boundary fields (Phase B).

---
+++ .lake/build/ir/Noperthedron/SolutionTable/Witness.c
@@ -0,0 +1,321 @@
+// Lean compiler output
+// Module: Noperthedron.SolutionTable.Witness
+// Imports: public import Init public import Noperthedron.SolutionTable.WitnessData
+#include <lean/lean.h>
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-label"
+#elif defined(__GNUC__) && !defined(__CLANG__)
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-label"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__5;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__10;
+static lean_object* lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4___closed__0;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__7;
+uint8_t lean_int_dec_le(lean_object*, lean_object*);
+static lean_object* lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2___closed__0;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__6;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__3;
+lean_object* lean_nat_to_int(lean_object*);
+static lean_object* lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2;
+extern lean_object* lp_Noperthedron_Solution_getLoadedTableImpl;
+lean_object* lean_array_fget(lean_object*, lean_object*);
+static lean_object* lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9___closed__0;
+static lean_object* lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+lean_object* lean_int_neg(lean_object*);
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8;
+static lean_object* lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0;
+LEAN_EXPORT uint8_t lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9;
+static lean_object* _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; 
+x_1 = lean_unsigned_to_nat(0u);
+x_2 = lp_Noperthedron_Solution_getLoadedTableImpl;
+x_3 = lean_array_fget(x_2, x_1);
+return x_3;
+}
+}
+static lean_object* _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; 
+x_1 = lean_unsigned_to_nat(0u);
+x_2 = lean_nat_to_int(x_1);
+return x_2;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; uint8_t x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 0);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = 0;
+x_5 = lean_box(x_4);
+x_6 = lean_apply_1(x_3, x_5);
+x_7 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1;
+x_8 = lean_int_dec_le(x_6, x_7);
+lean_dec(x_6);
+return x_8;
+}
+}
+static lean_object* _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2___closed__0() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; 
+x_1 = lean_unsigned_to_nat(6451200u);
+x_2 = lean_nat_to_int(x_1);
+return x_2;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; lean_object* x_4; uint8_t x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 1);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2___closed__0;
+x_5 = 0;
+x_6 = lean_box(x_5);
+x_7 = lean_apply_1(x_3, x_6);
+x_8 = lean_int_dec_le(x_4, x_7);
+lean_dec(x_7);
+return x_8;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__3() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; uint8_t x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 0);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = 1;
+x_5 = lean_box(x_4);
+x_6 = lean_apply_1(x_3, x_5);
+x_7 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1;
+x_8 = lean_int_dec_le(x_6, x_7);
+lean_dec(x_6);
+return x_8;
+}
+}
+static lean_object* _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4___closed__0() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; 
+x_1 = lean_unsigned_to_nat(48384000u);
+x_2 = lean_nat_to_int(x_1);
+return x_2;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; lean_object* x_4; uint8_t x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 1);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4___closed__0;
+x_5 = 1;
+x_6 = lean_box(x_5);
+x_7 = lean_apply_1(x_3, x_6);
+x_8 = lean_int_dec_le(x_4, x_7);
+lean_dec(x_7);
+return x_8;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__5() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; uint8_t x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 0);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = 2;
+x_5 = lean_box(x_4);
+x_6 = lean_apply_1(x_3, x_5);
+x_7 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1;
+x_8 = lean_int_dec_le(x_6, x_7);
+lean_dec(x_6);
+return x_8;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__6() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; lean_object* x_4; uint8_t x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 1);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2___closed__0;
+x_5 = 2;
+x_6 = lean_box(x_5);
+x_7 = lean_apply_1(x_3, x_6);
+x_8 = lean_int_dec_le(x_4, x_7);
+lean_dec(x_7);
+return x_8;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__7() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; uint8_t x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 0);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = 3;
+x_5 = lean_box(x_4);
+x_6 = lean_apply_1(x_3, x_5);
+x_7 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1;
+x_8 = lean_int_dec_le(x_6, x_7);
+lean_dec(x_6);
+return x_8;
+}
+}
+static lean_object* _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; 
+x_1 = lean_unsigned_to_nat(24268800u);
+x_2 = lean_nat_to_int(x_1);
+return x_2;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; lean_object* x_4; uint8_t x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 1);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0;
+x_5 = 3;
+x_6 = lean_box(x_5);
+x_7 = lean_apply_1(x_3, x_6);
+x_8 = lean_int_dec_le(x_4, x_7);
+lean_dec(x_7);
+return x_8;
+}
+}
+static lean_object* _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9___closed__0() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0;
+x_2 = lean_int_neg(x_1);
+return x_2;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; uint8_t x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 0);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = 4;
+x_5 = lean_box(x_4);
+x_6 = lean_apply_1(x_3, x_5);
+x_7 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9___closed__0;
+x_8 = lean_int_dec_le(x_6, x_7);
+lean_dec(x_6);
+return x_8;
+}
+}
+static uint8_t _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__10() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; lean_object* x_3; lean_object* x_4; uint8_t x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; 
+x_1 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0;
+x_2 = lean_ctor_get(x_1, 5);
+lean_inc_ref(x_2);
+x_3 = lean_ctor_get(x_2, 1);
+lean_inc_ref(x_3);
+lean_dec_ref(x_2);
+x_4 = lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0;
+x_5 = 4;
+x_6 = lean_box(x_5);
+x_7 = lean_apply_1(x_3, x_6);
+x_8 = lean_int_dec_le(x_4, x_7);
+lean_dec(x_7);
+return x_8;
+}
+}
+lean_object* initialize_Init(uint8_t builtin);
+lean_object* initialize_Noperthedron_Noperthedron_SolutionTable_WitnessData(uint8_t builtin);
+static bool _G_initialized = false;
+LEAN_EXPORT lean_object* initialize_Noperthedron_Noperthedron_SolutionTable_Witness(uint8_t builtin) {
+lean_object * res;
+if (_G_initialized) return lean_io_result_mk_ok(lean_box(0));
+_G_initialized = true;
+res = initialize_Init(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Noperthedron_Noperthedron_SolutionTable_WitnessData(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0();
+lean_mark_persistent(lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__0);
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1();
+lean_mark_persistent(lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1___closed__1);
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__1();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2___closed__0 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2___closed__0();
+lean_mark_persistent(lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2___closed__0);
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__2();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__3 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__3();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4___closed__0 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4___closed__0();
+lean_mark_persistent(lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4___closed__0);
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__4();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__5 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__5();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__6 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__6();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__7 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__7();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0();
+lean_mark_persistent(lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8___closed__0);
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__8();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9___closed__0 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9___closed__0();
+lean_mark_persistent(lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9___closed__0);
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__9();
+lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__10 = _init_lp_Noperthedron_Solution_solutionTable__covers___nativeDecide__1__10();
+return lean_io_result_mk_ok(lean_box(0));
+}
+#ifdef __cplusplus
+}
+#endif
