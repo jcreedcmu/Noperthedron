8e757247333d777aa7b923cd03146115fadb21f9
David Renshaw <dwrenshaw@gmail.com>
2026-05-02 10:13:17 -0400

    guard_msgs for valid_table_imples_exists_not_rupert

--- Noperthedron/ProofOfMainTheoremWithHole.lean
+++ Noperthedron/ProofOfMainTheoremWithHole.lean
@@ -7,7 +7,8 @@ namespace Noperthedron
 theorem valid_table_imples_exists_not_rupert (vtab : Solution.ValidTable) : ExistsNonRupertPolyhedron :=
   ⟨Noperthedron.exactVerts, interior_exactVerts_hull_nonempty, nopert_not_rupert vtab⟩
 
-/-
-Here we want to allow only [propext, Classical.choice, Quot.sound].
+/--
+info: 'Noperthedron.valid_table_imples_exists_not_rupert' depends on axioms: [propext, Classical.choice, Quot.sound]
 -/
+#guard_msgs in
 #print axioms valid_table_imples_exists_not_rupert
