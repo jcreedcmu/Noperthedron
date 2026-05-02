cf46833bf034c28b7bbdbbcd64289f18d6dbdb86
Jason Reed <jcreed@gmail.com>
2026-05-01 21:16:06 -0400

    a step towards getting InteriorNonempty talking about Euc(3)

--- Noperthedron/Vertices/InteriorNonempty.lean
+++ Noperthedron/Vertices/InteriorNonempty.lean
@@ -53,55 +53,68 @@ theorem M_rat_det_ne_zero : M_rat.det ≠ 0 := by
   simp [h] at h2
   exact_mod_cast h2
 
-def linearIndVerts : Set (Fin 3 → ℚ) := {2 * C1, C1 + C2, C1 + C3}
-def affineIndVerts : Set (Fin 3 → ℚ) := {-C1, C1, C2, C3}
+theorem M_real_det_ne_zero : (M_rat.map (Rat.cast : ℚ → ℝ)).det ≠ 0 := by
+  rw [← Rat.cast_det]; exact_mod_cast M_rat_det_ne_zero
+
+noncomputable def C1ℝ : Fin 3 → ℝ := fun i => (C1 i : ℝ)
+noncomputable def C2ℝ : Fin 3 → ℝ := fun i => (C2 i : ℝ)
+noncomputable def C3ℝ : Fin 3 → ℝ := fun i => (C3 i : ℝ)
+
+def linearIndVerts : Set (Fin 3 → ℝ) := {2 * C1ℝ, C1ℝ + C2ℝ, C1ℝ + C3ℝ}
 
 def linearIndVertsNonzero : ∀ v ∈ linearIndVerts, v ≠ 0 := by
   intro v hv
   simp [linearIndVerts] at hv
   obtain rfl | rfl | rfl := hv
-  · intro h; simp [C1] at h; apply_fun (· 0) at h; simp at h;
-  · intro h; simp [C1, C2] at h; apply_fun (· 0) at h; simp at h; norm_num at h
-  · intro h; simp [C1, C3] at h; apply_fun (· 0) at h; simp at h; norm_num at h
+  · intro h; apply_fun (· 0) at h; simp [C1ℝ, C1] at h
+  · intro h; apply_fun (· 0) at h; simp [C1ℝ, C2ℝ, C1, C2] at h; norm_num at h
+  · intro h; apply_fun (· 0) at h; simp [C1ℝ, C3ℝ, C1, C3] at h; norm_num at h
 
-theorem M_rat_cols_eq : M_rat.col = ![2 * C1, C1 + C2, C1 + C3] := by
-  ext i j; fin_cases i <;> fin_cases j <;>
-    simp [M_rat, C1, C2, C3, Matrix.col, d1, d2, n1a, n1b, n2a, n2b, n2c, n3a, n3b, n3c] <;> ring
+theorem M_real_cols_eq :
+    (M_rat.map (Rat.cast : ℚ → ℝ)).col = ![2 * C1ℝ, C1ℝ + C2ℝ, C1ℝ + C3ℝ] := by
+  ext i j; fin_cases i <;> fin_cases j
+  all_goals simp [Matrix.col, M_rat, C1ℝ, C2ℝ, C3ℝ, C1, C2, C3,
+          d1, d2, n1a, n1b, n2a, n2b, n2c, n3a, n3b, n3c]
+  all_goals ring_nf
 
-theorem linearIndVerts_hrange : Set.range ![2 * C1, C1 + C2, C1 + C3] = linearIndVerts := by
+theorem linearIndVerts_hrange :
+    Set.range ![2 * C1ℝ, C1ℝ + C2ℝ, C1ℝ + C3ℝ] = linearIndVerts := by
   simp only [Nat.succ_eq_add_one, Nat.reduceAdd, Matrix.range_cons, Matrix.range_empty,
     Set.union_empty, Set.union_singleton, Set.union_insert, linearIndVerts]
   ext
-  simp only [Set.mem_insert_iff, Set.mem_singleton_iff];
+  simp only [Set.mem_insert_iff, Set.mem_singleton_iff]
   tauto
 
 theorem linearInd_key :
-      LinearIndependent ℚ (fun v => v : linearIndVerts → Fin 3 → ℚ) := by
-  have hli := Matrix.linearIndependent_cols_of_det_ne_zero M_rat_det_ne_zero
-  rw [M_rat_cols_eq] at hli
+    LinearIndependent ℝ (fun v => v : linearIndVerts → Fin 3 → ℝ) := by
+  have hli := Matrix.linearIndependent_cols_of_det_ne_zero M_real_det_ne_zero
+  rw [M_real_cols_eq] at hli
   rw [linearIndependent_subtype_iff, ← linearIndVerts_hrange]
   exact (linearIndepOn_range_iff hli.injective id).mpr hli
 
 theorem affineInd_key :
-    AffineIndependent ℚ (fun p => p : ({-C1} ∪ (fun v => v +ᵥ (-C1)) '' linearIndVerts : Set (Fin 3 → ℚ))
-        → (Fin 3 → ℚ)) := by
-   rw [← linearIndependent_set_iff_affineIndependent_vadd_union_singleton]
-   · exact linearInd_key
-   · exact linearIndVertsNonzero
+    AffineIndependent ℝ (fun p => p :
+      ({-C1ℝ} ∪ (fun v => v +ᵥ (-C1ℝ)) '' linearIndVerts : Set (Fin 3 → ℝ)) → (Fin 3 → ℝ)) := by
+  rw [← linearIndependent_set_iff_affineIndependent_vadd_union_singleton]
+  · exact linearInd_key
+  · exact linearIndVertsNonzero
+
+def affineIndVertsR : Set (Fin 3 → ℝ) := {-C1ℝ, C1ℝ, C2ℝ, C3ℝ}
 
 lemma neg_c1_cup_eq_affineIndVerts :
-    ({-C1} ∪ (fun v => v +ᵥ (-C1)) '' linearIndVerts) = affineIndVerts := by
-  simp only [linearIndVerts, affineIndVerts, Set.image_insert_eq, Set.image_singleton,
+    ({-C1ℝ} ∪ (fun v => v +ᵥ (-C1ℝ)) '' linearIndVerts) = affineIndVertsR := by
+  simp only [linearIndVerts, affineIndVertsR, Set.image_insert_eq, Set.image_singleton,
              vadd_eq_add, Set.union_insert, Set.union_singleton]
   ring_nf
   ext
   simp only [Set.mem_insert_iff, Set.mem_singleton_iff]
   tauto
 
-theorem affineIndVertsAffine : AffineIndependent ℚ (fun p => p : affineIndVerts → (Fin 3 → ℚ)) := by
+theorem affineIndVertsAffine :
+    AffineIndependent ℝ (fun p => p : affineIndVertsR → (Fin 3 → ℝ)) := by
   rw [← neg_c1_cup_eq_affineIndVerts]
   exact affineInd_key
 
-theorem interior_exactVerts_null_nonempty :
+theorem interior_exactVerts_hull_nonempty :
     (interior ((convexHull ℝ) (exactVerts : Set (Euc(3))))).Nonempty := by
   sorry
